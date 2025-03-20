4337f7ef7370f146fc8048b4177392600b1739c8
godinezsteven1 <steven.godin.42@gmail.com>
2025-03-19 23:41:20 -0400

    initialized NLP portion of project, word segmentation, out of 3 others (HMM, spell check, word segmentation, database

---
+++ NLP/wordSegm.py
@@ -0,0 +1,10 @@
+# This file is meant for the word segmentation of the NLP
+# The goal of this file is to be able to split the words accurately and correctly. 
+
+import math
+
+#uses Viterbi Algorithm to find most likely sequence 
+#runs it with (maybe corpus) dataset csv postgreSQl table with word: frequency 
+
+    
+    
