9656cc17eca05a480e3b029d28ba1e14a9fbdb12
godinezsteven1 <steven.godin.42@gmail.com>
2025-03-22 01:52:35 -0400

    created and added comments to NLP file, HMM with its respective description

---
+++ NLP/HMM.py
@@ -0,0 +1,4 @@
+# This HMM class is made for probability and likilhood of word sequence
+#given hidden states of user input 
+# uses states observations emission probabilities for the HMM 
+# uses Viterbi algorithm, a brute force algo, to find the most likely sequence of words 
