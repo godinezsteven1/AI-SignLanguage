279fc0dd76b93cde5c8ac452268b97778c40b3bb
godinezsteven1 <steven.godin.42@gmail.com>
2025-03-24 13:12:07 -0400

    fixed issue 'HMM.py #2', moved return statement outside of loop/fixed indentation

--- NLP/HMM.py
+++ NLP/HMM.py
@@ -16,25 +16,27 @@ class HMM:
         V = [{}] #probability at each step 
         path = {} #sequence 
 
-        for y in states:
-            V[0][y] = startProb[y] * emissionProb[y][obs[0]] #first observation 
+        for y in self.states:
+            V[0][y] = self.startProb[y] * self.emissionProb[y][obs[0]] #first observation 
             path[y] = [y]
         for t in range(1,len(obs)):
             V.append({})
             newPath = {}
-            for y in states:
+            for y in self.states:
                 # account for all previous 
-                (prob, state) = max([(V[t - 1][y0] * transitionProb[y0][y] * emissionProb[y0][obs[t]], y0) for y0 in states])
+                (prob, state) = max([(V[t - 1][y0] * self.transitionProb[y0][y] * self.emissionProb[y0][obs[t]], y0) for y0 in self.states])
                 #store max prob 
                 V[t][y] = prob
                 newPath[y] = path[state] + [y] #updates path 
                 path = newPath # updates path directory 
-            (prob, state) = max([(V[-1][y], y) for y in states]) # state w highest prob in final observation 
+            (prob, state) = max([(V[-1][y], y) for y in self.states]) # state w highest prob in final observation 
             #return (prob, path[state]) # return prob w its state
             #print(prob, path[state]) # print 
-            return path[state] # returns most likely obvservation sequence 
+        return path[state] # returns most likely obvservation sequence 
             
     #states = all states of model?
     #obs = user input
     #prob yet to be calculated 
                 
+#need to add start probabilities, transition probabilities, emissin probabilities with a reddit webscrapper
+# needs to filter out NSFW textg 
