b22950d0002d9abc98c402468f7b52bd5f209956
godinezsteven1 <steven.godin.42@gmail.com>
2025-04-12 20:33:08 -0400

    #16 solved.. working on #11 (finishing up) text segmentation based on dataset created from reddit

--- NLP/wordSegm.py
+++ NLP/wordSegm.py
@@ -1,10 +1,42 @@
 # This file is meant for the word segmentation of the NLP
 # The goal of this file is to be able to split the words accurately and correctly. 
 import math
+import json
+from collections import defaultdict
+
+startProbFileName = "startProb.json"
+transitionProbName = "transitionProb.json"
+
+# Reading our start probabilities 
+def loadStartProbabilities(fileName=startProbFileName): 
+    with open(fileName, 'r') as file:
+        return json.load(file)
+    print(f"{fileName} read")
+
+    
+# Reading our transition probabilities and create nested dict for easy access
+def loadTransitionProbabilitiesCreateNestedDict(fileName=transitionProbName): 
+    with open(fileName, 'r') as file:
+        transitions = json.load(file)
+    print(f"{fileName} read")
+
+    nestedDict = {}
+    for word, nextWords in transitions.items():
+        nestedDict[word] = defaultdict(lambda: 1e-10, nextWords) # chat helped w the nameless func 
+    # so my computer and urs wont explode - aka if words dont exist because my dataset is from reddit + lightweight
+    answer = defaultdict(lambda: defaultdict(lambda: 1e-10)) # give it small probability 
+    answer.update(nestedDict)
+
+    return answer
+
+
+
+
+
 
 #uses Viterbi Algorithm to find most likely sequence 
 #runs it with (maybe corpus) dataset csv postgreSQl table with word: frequency 
-def textSegmentation(text, wordProb):
+def textSegmentation(text, wordProb=None):
     textLength = len(text)
     #tuple from -inf to text length for (word, probability) or technically (%, word)
     # if -inf = no valid seg found for now 
@@ -24,6 +56,8 @@ def textSegmentation(text, wordProb):
             
         #need to go to the word endings 
         for j in range(i + 1, n + 1):
+
+        
         #try all combinations 
         #does word exist chcek from csv?
         #log word for probabilities 
